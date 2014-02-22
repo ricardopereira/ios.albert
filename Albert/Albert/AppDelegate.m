d9c0c45b9118695a556e6a8d71be1e93443d71fc
Ricardo Pereira <rikpereira@gmail.com>
2014-02-22 03:50:08 +0000

    Base layout

--- Albert/Albert/AppDelegate.m
+++ Albert/Albert/AppDelegate.m
@@ -61,8 +61,11 @@
 # pragma mark Helper
 
 - (void)customizeUI {
-    //Albert color
+    // Albert color
     UIColor *colorAlbert = [UIColor colorWithRed:113/255.0 green:82/255.0 blue:105/255.0 alpha:1.0];
+
+    // Customize status bar
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
 
     // Customize NavBar
     [[UINavigationBar appearance] setBarTintColor:colorAlbert];
